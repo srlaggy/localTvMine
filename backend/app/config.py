d786341c37be65f8c65d4aa57498e6c3455f12bd
ignacio.ulloa <ignacio@uqomm.com>
2026-06-11 11:19:34 -0400

    add dynamic ip support for server

--- backend/app/config.py
+++ backend/app/config.py
@@ -4,6 +4,7 @@ class Settings(BaseSettings):
     DATABASE_URL: str = "sqlite:///./bustaTv.db"
     SECRET_API_KEY: str = "bustatv-dev-secret-key-changeme"
     BACKEND_URL: str = "http://localhost:8000"
+    HOST_IP: str = "localhost"
 
     class Config:
         env_file = ".env"
